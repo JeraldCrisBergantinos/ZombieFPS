2389d76186f8b704f63b6c41d7f6fa6fe570e8e0
JeraldCrisBergantinos <44352027+JeraldCrisBergantinos@users.noreply.github.com>
2023-06-27 18:21:28 +0800

    First commit

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Making sure my mouse does not leave the game window.
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        // Making sure we can only look 90 degrees up and 90 degress down and don't get sick.
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector2.up * mouseX);
+    }
+}
